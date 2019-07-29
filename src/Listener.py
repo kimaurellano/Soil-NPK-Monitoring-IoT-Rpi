859c3bfaeb5b3e6e9d0ec67bf25f15bf80d6e6f2
kimaurellano <kim.aurellano@yahoo.com>
2019-07-29 13:41:17 +0800

    Added time validation before insertion

--- src/Listener.py
+++ src/Listener.py
@@ -8,7 +8,7 @@ import paho.mqtt.client as mqtt
 from datetime import datetime
 
 # IP Address of the broker(Rpi)
-MQTT_Broker = "192.168.31.154"
+MQTT_Broker = "192.168.0.105"
 # Common port
 MQTT_Port = 1883
 
@@ -22,47 +22,55 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     # datetime object containing current date and time
     now = datetime.now()
-    
+	
     if msg.payload != "":
         print("Recieved.")
-        try:
-            # Connection
-            connection = mysql.connector.connect(
-            host='localhost',
-            database='mysql',
-            user='root',
-            password='admin123')
-            
-            # Data insertion
-            dbcursor = connection.cursor()
-            dbcursor.execute("select * from Soil_Data");
-            dbcursor.fetchall()
-            print("rows:{}".format(dbcursor.rowcount))
-            currentrowcount = dbcursor.rowcount + 1;
-            print(currentrowcount)
-            
-            # Extract JSON content
-            jsonDoc = json.loads(msg.payload)
-            print(jsonDoc['SensorID'])
-            
-            # dd/mm/YY H:M:S
-            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            
-            # Sql query
-            sql_insert_query = "insert into Soil_Data values({}, '{}', '{}', {}, {}, {})".format(currentrowcount, jsonDoc['SensorID'], dt_string, jsonDoc['Nitrogen'], jsonDoc['Phosphorous'], jsonDoc['Potassium'])
-            
-            dbcursor.execute(sql_insert_query)
-            
-            # Apply changes
-            connection.commit()
-            
-            dbcursor.close()
-            connection.close()
-            
-            print("Insertion successful")
-        except mysql.connector.Error as error:
-            connection.rollback()
-            print("Insertion failed:" + str(error))
+		parsedTime = now.strftime("%H:%M:%S")
+	
+		hour = int(parsedTime.split(':')[0])
+		min = int(parsedTime.split(':')[1])
+		sec = int(parsedTime.split(':')[2])
+		
+		# Prevent logging the data
+		if(min <> 0 and sec <> 0):
+			print("Not the time for data insertion.")
+		else:
+			try:
+				# Connection.
+				connection = mysql.connector.connect(
+				host='2.57.89.1',
+				database='u690747639_soil',
+				user='u690747639_root',
+				password='0OzJ2dik0HEZ')
+				
+				# Data insertion
+				dbcursor = connection.cursor()
+				dbcursor.execute("SELECT * FROM soil_data");
+				dbcursor.fetchall()
+				print("rows:{}".format(dbcursor.rowcount))
+				
+				# Extract JSON content
+				jsonDoc = json.loads(msg.payload)
+				print(jsonDoc['SensorID'])
+				
+				# dd/mm/YY H:M:S
+				dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+				
+				# Sql query
+				sql_insert_query = "insert into Soil_Data values(NULL, '{}', '{}', {}, {}, {})".format(currentrowcount, jsonDoc['SensorID'], dt_string, jsonDoc['Nitrogen'], jsonDoc['Phosphorous'], jsonDoc['Potassium'])
+				
+				dbcursor.execute(sql_insert_query)
+				
+				# Apply changes
+				connection.commit()
+				
+				dbcursor.close()
+				connection.close()
+				
+				print("Insertion successful")
+			except mysql.connector.Error as error:
+				connection.rollback()
+				print("Insertion failed:" + str(error))
 
 # Events to listen
 client = mqtt.Client()
