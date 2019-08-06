2847fe0640c4397a910a7cc299720d34994bbd8f
kimaurellano <kim.aurellano@yahoo.com>
2019-08-06 14:10:08 +0800

    Added missing node check

--- src/Listener.py
+++ src/Listener.py
@@ -2,67 +2,103 @@ import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
 
+import threading
 import json
 import paho.mqtt.client as mqtt
 
 from datetime import datetime
 
 # IP Address of the broker(Rpi)
-MQTT_Broker = "192.168.31.154"
+MQTT_Broker = "192.168.0.105"
 # Common port
 MQTT_Port = 1883
-
-Keep_Alive_Interval = 45
+# Connection lifespan
+Keep_Alive_Interval = 60
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    
-    client.subscribe("esp8266")
-    
+
+    # Topics to listen from
+    client.subscribe("esp8266/#")
+
+
 def on_message(client, userdata, msg):
-    # datetime object containing current date and time
-    now = datetime.now()
+    print(msg.payload)
     
-    if msg.payload != "":
-        print("Recieved.")
-        try:
-            # Connection
-            connection = mysql.connector.connect(
-            host='localhost',
-            database='mysql',
-            user='root',
-            password='admin123')
-            
-            # Data insertion
-            dbcursor = connection.cursor()
-            dbcursor.execute("select * from Soil_Data");
-            dbcursor.fetchall()
-            print("rows:{}".format(dbcursor.rowcount))
-            currentrowcount = dbcursor.rowcount + 1;
-            print(currentrowcount)
-            
-            # Extract JSON content
-            jsonDoc = json.loads(msg.payload)
-            print(jsonDoc['SensorID'])
-            
-            # dd/mm/YY H:M:S
-            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            
-            # Sql query
-            sql_insert_query = "insert into Soil_Data values({}, '{}', '{}', {}, {}, {})".format(currentrowcount, jsonDoc['SensorID'], dt_string, jsonDoc['Nitrogen'], jsonDoc['Phosphorous'], jsonDoc['Potassium'])
-            
-            dbcursor.execute(sql_insert_query)
-            
-            # Apply changes
-            connection.commit()
-            
-            dbcursor.close()
-            connection.close()
-            
-            print("Insertion successful")
-        except mysql.connector.Error as error:
-            connection.rollback()
-            print("Insertion failed:" + str(error))
+    # Pass payload from NodeMCUs
+    get_data(msg)
+
+
+seconds = 0
+def timeout_timer():
+	if seconds > 30:
+		# TODO
+	seconds += 1
+	print("{}s".format(seconds))
+	
+# Init the timer	
+timer = threading.Timer(1.0, timeout_timer)
+
+# Nodes we have to look for
+nodes = ["Node-1", "Node-2", "Node-3", "Node-4", "Node-5", "Node-6"]
+i = 0
+
+def get_data(msg):
+    print("Recieved.")
+    try:
+        # Connection to remote webserver
+        connection = mysql.connector.connect(
+            host='2.57.89.1',
+            database='u690747639_soil',
+            user='u690747639_root',
+            password='0OzJ2dik0HEZ')
+
+        # Data insertion
+        dbcursor = connection.cursor()
+        dbcursor.execute("select * from soil_data")
+        dbcursor.fetchall()
+        print("rows:{}".format(dbcursor.rowcount))
+        currentrowcount = dbcursor.rowcount + 1
+        print(currentrowcount)
+
+        # Extract JSON content
+        jsonDoc = json.loads(msg.payload)
+        print(jsonDoc['SensorID'])
+		
+		if jsonDoc['SensorID'] == list.index(i):
+			# Reset timer
+			seconds = 0
+			
+
+        # datetime object containing current date and time
+        now = datetime.now()
+        dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
+
+        # Sql query
+        sql_insert_query = "insert into soil_data values(NULL, '{}', '{}', {}, {}, {}, {}, {}, {})".format(
+            jsonDoc['SensorID'],
+            dt_string,
+            jsonDoc['Nitrogen'],
+            jsonDoc['Phosphorous'],
+            jsonDoc['Potassium'],
+            jsonDoc['Nitrogen_FRQ'],
+            jsonDoc['Phosphorous_FRQ'],
+            jsonDoc['Potassium_FRQ'])
+
+        dbcursor.execute(sql_insert_query)
+
+        # Apply changes
+        connection.commit()
+        
+        # Dispose connection after use
+        connection.close()
+        dbcursor.close()
+
+        print("Insertion successful from {}".format(jsonDoc['SensorID']))
+    except mysql.connector.Error as error:
+        connection.rollback()
+        print("Insertion failed:" + str(error))
+
 
 # Events to listen
 client = mqtt.Client()
@@ -74,3 +110,4 @@ client.connect(MQTT_Broker, MQTT_Port, Keep_Alive_Interval)
 
 # Continous listening to the broker
 client.loop_forever()
+
