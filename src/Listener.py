360171cd8f46eedcb15f22eba374ba04d685811e
kimaurellano <kim.aurellano@yahoo.com>
2019-08-14 23:04:26 +0800

    Fix bug where if last node still no value but continues to proceed

--- src/Listener.py
+++ src/Listener.py
@@ -20,8 +20,8 @@ Keep_Alive_Interval = 60
 referenceNodes = ["Node-1", "Node-2", "Node-3", "Node-4", "Node-5", "Node-6"]
 # Array index
 i = 0
-# The node to get from payload
-payloadNode = {}
+# The last successful data capture at certain node
+lastData = {}
 # Tries
 tryCount = 0
 #
@@ -36,8 +36,6 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    # print(msg.payload)
-
     jsonDoc = json.loads(msg.payload)
 
     # Pass payload from NodeMCUs
@@ -45,14 +43,95 @@ def on_message(client, userdata, msg):
 
 
 def get_data(jsonDoc):
-    global i, tryCount, missedNodes
+    global i, tryCount, missedNodes, lastData, client
     print("Recieved node:{}".format(jsonDoc["SensorID"]))
     print("# of tries:{} with {}".format(tryCount, referenceNodes[i]))
     print("# of missed nodes:{} \n".format(missedNodes))
     tryCount += 1
 
+    if tryCount > 10 and referenceNodes[i] == "Node-6":
+        print("WARNING: Last node is reached and still no data.\nAttempting to fill with the last captured data...\n")
+
+        client.disconnect()
+
+        missedNodes += 1
+
+        currentNode = i
+
+        # Back to Node-1
+        i = 0
+
+        while missedNodes > 0:
+            print("Auto filling missed nodes:{}".format(missedNodes))
+
+            # Insert data for missed nodes
+            print("Starting data insertion of sensorid:{}".format(
+                referenceNodes[currentNode]))
+
+            try:
+                # Connection to remote webserver
+                connection = mysql.connector.connect(
+                    host='2.57.89.1',
+                    database='u690747639_soil',
+                    user='u690747639_root',
+                    password='upNhZDv5NBu756is')
+
+                # Error check
+                if connection.is_connected:
+                    print("Successfully connected to MySQL")
+                else:
+                    print("Connection failed")
+
+                # Current row check
+                dbcursor = connection.cursor()
+                dbcursor.execute("select * from soil_data")
+                dbcursor.fetchall()
+                print("rows:{}".format(dbcursor.rowcount))
+
+                now = datetime.now()
+                curDate = now.strftime("%d-%m-%Y %H:%M:%S")
+
+                # Sql query
+                sql_insert_query = "insert into soil_data values(NULL, '{}', '{}', {}, {}, {}, {}, {}, {}, '{}')".format(
+                    referenceNodes[i],
+                    curDate,
+                    lastData['Nitrogen'],
+                    lastData['Phosphorous'],
+                    lastData['Potassium'],
+                    lastData['Nitrogen_FRQ'],
+                    lastData['Phosphorous_FRQ'],
+                    lastData['Potassium_FRQ'],
+                    "PROCEDURALLY-FILLED")
+
+                # Execute query
+                dbcursor.execute(sql_insert_query)
+
+                # Apply changes by the executed query
+                connection.commit()
+
+                # Dispose connection after use
+                connection.close()
+                dbcursor.close()
+            except mysql.connector.Error as error:
+                connection.rollback()
+                print("Insertion failed:" + str(error))
+
+            missedNodes -= 1
+
+            currentNode -= 1
+        else:
+            tryCount = 0
+
+            # Reconnect
+            client.connect(MQTT_Broker, MQTT_Port, Keep_Alive_Interval)
+
+            print("All missed nodes are filled.\n")
+
     if tryCount < 10 and jsonDoc["SensorID"] == referenceNodes[i]:
-        print("Attempt of insertion...")
+        #
+        client.disconnect()
+
+        print("NODE MATCH! Attempt of insertion...")
 
         # Reset try counts
         tryCount = 0
@@ -108,7 +187,10 @@ def get_data(jsonDoc):
             connection.rollback()
             print("Insertion failed:" + str(error))
 
-        # Remember to current node
+        # Capture the successful data for reserve
+        lastData = jsonDoc
+
+        # Remember the current node
         currentNode = i
 
         # Proceed to next node
@@ -174,9 +256,13 @@ def get_data(jsonDoc):
             # We have filled the missed nodes
             missedNodes -= 1
         else:
-            print("All missed nodes are filled. Proceeding to next node...")
+            # Reconnect
+            client.connect(MQTT_Broker, MQTT_Port, Keep_Alive_Interval)
+
+            print("All missed nodes are filled. Proceeding to {}...\n".format(
+                referenceNodes[i]))
     elif tryCount > 10 and jsonDoc["SensorID"] != referenceNodes[i]:
-        print("Proceeding to next node")
+        print("Proceeding to next node...\n")
 
         # Proceed to next node
         i += 1
@@ -188,7 +274,7 @@ def get_data(jsonDoc):
         missedNodes += 1
 
     # We only have 6 nodes
-    if i > 6:
+    if i > 5:
         i = 0
 
 
