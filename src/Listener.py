8e7e55b18c8fb8926343a48d3165988923495a68
kimaurellano <kim.aurellano@yahoo.com>
2019-08-14 11:53:38 +0800

    Removed insertion function

--- src/Listener.py
+++ src/Listener.py
@@ -10,7 +10,7 @@ import threading
 from datetime import datetime
 
 # IP Address of the broker(Rpi)
-MQTT_Broker = "192.168.31.51"
+MQTT_Broker = "192.168.0.105"
 # Common port
 MQTT_Port = 1883
 # Connection lifespan
@@ -38,43 +38,154 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     # print(msg.payload)
 
+    jsonDoc = json.loads(msg.payload)
+
     # Pass payload from NodeMCUs
-    get_data(msg)
+    get_data(jsonDoc)
 
 
-def get_data(msg):
-    print("Recieved.")
-
-    # Extract payload as JSON format
-    jsonDoc = json.load(msg.payload)
-
+def get_data(jsonDoc):
     global i, tryCount, missedNodes
+    print("Recieved node:{}".format(jsonDoc["SensorID"]))
+    print("# of tries:{} with {}".format(tryCount, referenceNodes[i]))
+    print("# of missed nodes:{} \n".format(missedNodes))
+    tryCount += 1
+
     if tryCount < 10 and jsonDoc["SensorID"] == referenceNodes[i]:
+        print("Attempt of insertion...")
+
         # Reset try counts
         tryCount = 0
 
         # Insert data with the current payload
-        data_insertion(jsonDoc, jsonDoc["SensorID"])
+        print("Starting data insertion of sensorid:{}".format(
+            referenceNodes[i]))
+        try:
+            # Connection to remote webserver
+            connection = mysql.connector.connect(
+                host='2.57.89.1',
+                database='u690747639_soil',
+                user='u690747639_root',
+                password='upNhZDv5NBu756is')
+
+            # Error check
+            if connection.is_connected:
+                print("Successfully connected to MySQL")
+            else:
+                print("Connection failed")
+
+            # Current row check
+            dbcursor = connection.cursor()
+            dbcursor.execute("select * from soil_data")
+            dbcursor.fetchall()
+            print("rows:{}".format(dbcursor.rowcount))
+
+            now = datetime.now()
+            curDate = now.strftime("%d-%m-%Y %H:%M:%S")
+
+            # Sql query
+            sql_insert_query = "insert into soil_data values(NULL, '{}', '{}', {}, {}, {}, {}, {}, {}, '{}')".format(
+                referenceNodes[i],
+                curDate,
+                jsonDoc['Nitrogen'],
+                jsonDoc['Phosphorous'],
+                jsonDoc['Potassium'],
+                jsonDoc['Nitrogen_FRQ'],
+                jsonDoc['Phosphorous_FRQ'],
+                jsonDoc['Potassium_FRQ'],
+                "NORMALLY-FILLED")
+
+            # Execute query
+            dbcursor.execute(sql_insert_query)
+
+            # Apply changes by the executed query
+            connection.commit()
+
+            # Dispose connection after use
+            connection.close()
+            dbcursor.close()
+        except mysql.connector.Error as error:
+            connection.rollback()
+            print("Insertion failed:" + str(error))
+
+        # Remember to current node
+        currentNode = i
+
+        # Proceed to next node
+        i += 1
 
         while missedNodes > 0:
+            print("Auto filling missed nodes:{}".format(missedNodes))
+
             # Until no missing nodes left
-            i -= 1
+            currentNode -= 1
 
             # Insert data for missed nodes
-            data_insertion(jsonDoc, referenceNodes[i], "VALID")
+            print("Starting data insertion of sensorid:{}".format(
+                referenceNodes[currentNode]))
+            try:
+                # Connection to remote webserver
+                connection = mysql.connector.connect(
+                    host='2.57.89.1',
+                    database='u690747639_soil',
+                    user='u690747639_root',
+                    password='upNhZDv5NBu756is')
+
+                # Error check
+                if connection.is_connected:
+                    print("Successfully connected to MySQL")
+                else:
+                    print("Connection failed")
+
+                # Current row check
+                dbcursor = connection.cursor()
+                dbcursor.execute("select * from soil_data")
+                dbcursor.fetchall()
+                print("rows:{}".format(dbcursor.rowcount))
+
+                now = datetime.now()
+                curDate = now.strftime("%d-%m-%Y %H:%M:%S")
+
+                # Sql query
+                sql_insert_query = "insert into soil_data values(NULL, '{}', '{}', {}, {}, {}, {}, {}, {}, '{}')".format(
+                    referenceNodes[currentNode],
+                    curDate,
+                    jsonDoc['Nitrogen'],
+                    jsonDoc['Phosphorous'],
+                    jsonDoc['Potassium'],
+                    jsonDoc['Nitrogen_FRQ'],
+                    jsonDoc['Phosphorous_FRQ'],
+                    jsonDoc['Potassium_FRQ'],
+                    "PROCEDURALLY-FILLED")
+
+                # Execute query
+                dbcursor.execute(sql_insert_query)
+
+                # Apply changes by the executed query
+                connection.commit()
+
+                # Dispose connection after use
+                connection.close()
+                dbcursor.close()
+            except mysql.connector.Error as error:
+                connection.rollback()
+                print("Insertion failed:" + str(error))
 
             # We have filled the missed nodes
             missedNodes -= 1
-
+        else:
+            print("All missed nodes are filled. Proceeding to next node...")
     elif tryCount > 10 and jsonDoc["SensorID"] != referenceNodes[i]:
+        print("Proceeding to next node")
+
+        # Proceed to next node
+        i += 1
+
+        # Reset try counts
+        tryCount = 0
+
         # Remember what nodes are missed by identifying the preceeding index
         missedNodes += 1
-
-        # SUGGESTION: that if tries exceeded 10 times, should signal node as "STOPPED WORKING"
-        #data_insertion(jsonDoc, referenceNodes[i], "INVALID")
-
-    # Proceed to next node
-    i += 1
 
     # We only have 6 nodes
     if i > 6:
@@ -94,13 +205,14 @@ client.loop_forever()
 
 
 def data_insertion(data, sensorid, state):
+    print("Starting data insertion of sensorid:{}".format(sensorid))
     try:
         # Connection to remote webserver
         connection = mysql.connector.connect(
-            host='127.0.0.1',
-            database='mysql',
-            user='root',
-            password='')
+            host='2.57.89.1',
+            database='u690747639_soil',
+            user='u690747639_root',
+            password='upNhZDv5NBu756is')
 
         # Error check
         if connection.is_connected:
